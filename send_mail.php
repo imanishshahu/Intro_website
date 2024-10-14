7c3485bdc05bf5d61f492bb36aa065447404d0c1
imanishshahu <shahamanish7@gmail.com>
2024-10-14 15:49:18 +0545

    Add

---
+++ send_mail.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    // Email to send the form data to
+    $to = 'shahamanish7@gmail.com';
+    
+    // Email subject
+    $email_subject = "Contact Form: " . $subject;
+    
+    // Email body content
+    $email_body = "You have received a new message from the contact form.\n\n".
+                  "Here are the details:\n\nName: $name\n\nEmail: $email\n\nSubject: $subject\n\nMessage:\n$message";
+    
+    // Headers
+    $headers = "From: $email\n";
+    $headers .= "Reply-To: $email";
+
+    // Send email
+    if (mail($to, $email_subject, $email_body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "There was an error sending your message. Please try again.";
+    }
+}
+?>
